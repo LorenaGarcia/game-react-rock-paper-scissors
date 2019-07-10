1ede88e4727b68b37ba815091d70ad2327d96765
Lorena Garcia <lorena.garcia@element.com.mx>
2019-07-10 09:46:42 -0700

    Updates

---
+++ precache-manifest.476c29e9d16b7c258649aa8a15525a83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb8614df30c5ca01cabf80908e444229",
+    "url": "/game-react-rock-paper-scissors/index.html"
+  },
+  {
+    "revision": "76272dd0a0d3473b5967",
+    "url": "/game-react-rock-paper-scissors/static/css/main.2362604b.chunk.css"
+  },
+  {
+    "revision": "8f31488ca1054253a203",
+    "url": "/game-react-rock-paper-scissors/static/js/2.d6e98537.chunk.js"
+  },
+  {
+    "revision": "76272dd0a0d3473b5967",
+    "url": "/game-react-rock-paper-scissors/static/js/main.3aaef3ad.chunk.js"
+  },
+  {
+    "revision": "d9f7fbeef1713c92a75c",
+    "url": "/game-react-rock-paper-scissors/static/js/runtime~main.dcf1e94a.js"
+  }
+]);
